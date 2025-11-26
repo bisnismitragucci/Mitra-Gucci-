d455bd453a36ae6d50834d591ccf385a68c50c7c
bisnismitragucci <juliana81278616@gmail.com>
2025-11-26 15:21:12 +0700

    feat: Update hero video URL
    Replaces the old hero video URL with the new, requested version for improved visual content.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,7 +11,7 @@ const Hero: React.FC<HeroProps> = ({ t }) => {
   const waLink = "https://wa.me/6281325808529?text=Hallo%20saya%20ingin%20bergabung%20dan%20mendaftar";
   
   // URL Video (Versi yang diminta)
-  const videoUrl = "https://house-fastly-signed-eu-west-1-prod.brightcovecdn.com/media/v1/pmp4/static/clear/2924921183001/71f1d2ac-bcc9-4210-bd04-9f2c9c856224/33cb203f-4057-4143-ba70-dfadc0ca402a/main.mp4?fastly_token=NjkyNmRhNThfM2M4NjZlOWU5YTU2OWJkMGQyMWRjYzA3ODA1YTJkMDdjYzg1MTg4YmI1NjlhMzExZmRmZGQ0NTQ5ZTBlZjJkMl8vL2hvdXNlLWZhc3RseS1zaWduZWQtZXUtd2VzdC0xLXByb2QuYnJpZ2h0Y292ZWNkbi5jb20vbWVkaWEvdjEvcG1wNC9zdGF0aWMvY2xlYXIvMjkyNDkyMTE4MzAwMS83MWYxZDJhYy1iY2M5LTQyMTAtYmQwNC05ZjJjOWM4NTYyMjQvMzNjYjIwM2YtNDA1Ny00MTQzLWJhNzAtZGZhZGMwY2E0MDJhL21haW4ubXA0";
+  const videoUrl = "https://house-fastly-signed-eu-west-1-prod.brightcovecdn.com/media/v1/pmp4/static/clear/2924921183001/15f754d2-c039-4d97-a401-82d1b378e7f4/ac5a26d0-7ad1-44d5-ad3d-7fcb5d22c912/main.mp4?fastly_token=NjkyNzBiZGFfYjI5MDVlZWFlMzBlZDcwN2RmMmM2ZTJhYzMyMjJjYWM4ZTkyYWY3Mjc3MTdiYmZhYmNhZDNlMzI5OGQ5ODc3OF8vL2hvdXNlLWZhc3RseS1zaWduZWQtZXUtd2VzdC0xLXByb2QuYnJpZ2h0Y292ZWNkbi5jb20vbWVkaWEvdjEvcG1wNC9zdGF0aWMvY2xlYXIvMjkyNDkyMTE4MzAwMS8xNWY3NTRkMi1jMDM5LTRkOTctYTQwMS04MmQxYjM3OGU3ZjQvYWM1YTI2ZDAtN2FkMS00NGQ1LWFkM2QtN2ZjYjVkMjJjOTEyL21haW4ubXA0";
   
   return (
     <div className="relative w-full h-[60vh] md:h-[80vh] overflow-hidden bg-black">
