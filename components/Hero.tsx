1971a2c9ebdde92201b8212d8f523dbb365165a4
bisnismitragucci <juliana81278616@gmail.com>
2025-11-29 09:33:04 +0700

    feat(hero): Update background image URL
    Replaces the existing background image URL in the Hero component with a new one featuring a "White House/Gucci Exterior" theme, as requested.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,8 +10,8 @@ interface HeroProps {
 const Hero: React.FC<HeroProps> = ({ t }) => {
   const waLink = "https://wa.me/6281374192171?text=Hallo%20saya%20ingin%20bergabung%20dan%20mendaftar";
   
-  // URL Gambar Background Baru sesuai permintaan ("Fokus di sini")
-  const bgImage = "https://i.pinimg.com/1200x/fb/03/e5/fb03e5edf1ed52e9bdf6dcb64f497864.jpg";
+  // URL Gambar Background Baru (Gedung Putih/Gucci Exterior)
+  const bgImage = "https://i.pinimg.com/1200x/f4/8a/29/f48a2941b1d34d86360343f926ff57f3.jpg";
 
   return (
     <div className="relative w-full h-[60vh] md:h-[80vh] overflow-hidden bg-black">
